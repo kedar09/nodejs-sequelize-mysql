0c80f6f7ac6a151463f2e1a72b4262b8f51edd80
httpskedar <32976533+httpskedar@users.noreply.github.com>
2020-05-15 00:55:27 +0530

    Update author.model.js

--- app/Models/Author/author.model.js
+++ app/Models/Author/author.model.js
@@ -1,17 +1,17 @@
 module.exports = (sequelize, Sequelize) => {
-  const Author = sequelize.define('Author', {
-    authorId: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    authorName: {
-        type: Sequelize.STRING,
-        unique: true
-    }
-  },{
+    const Author = sequelize.define('Author', {
+        authorId: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        authorName: {
+            type: Sequelize.STRING,
+            unique: true
+        }
+    }, {
         tableName: 'author',
         timestamps: true
-  });
-  return Author;
-}
+    });
+    return Author;
+};
