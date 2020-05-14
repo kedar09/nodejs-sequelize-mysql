74385cd33a9795d9b04a9421f68b7095a7a70d37
httpskedar <32976533+httpskedar@users.noreply.github.com>
2020-05-15 00:58:04 +0530

    Update author.js

--- routes/author.js
+++ routes/author.js
@@ -1,19 +1,20 @@
-var express = require('express');
-var router = express.Router();
-var authorValidator = require('../app/Components/AuthorComponent/author.validator');
+const express = require('express');
+const router = express.Router();
+const authorValidator = require('../app/Components/AuthorComponent/author.validator');
 
-// Login user - (body: [email,password]) - (email,password is required)
+// Add Author - (body: [authorName]) - (authorName is required)
 router.post('/addAuthor', authorValidator.addAuthor);
 
-// Login user - (body: [email,password]) - (email,password is required)
+// Update Author - (body: [authorName, authorId]) - (authorName,authorId is required)
 router.put('/updateAuthor', authorValidator.updateAuthor);
 
-// Login user - (body: [email,password]) - (email,password is required)
+// Delete Author - (body: [authorId]) - (authorId is required)
 router.delete('/deleteAuthor', authorValidator.deleteAuthor);
 
-// register new user - (body: [email,password,displayName,phoneNumber]) - (email,password is required)
-router.post('/getAuthor', authorValidator.getAuthor);
+// Get All Author 
+router.get('/getAllAuthor', authorValidator.getAllAuthor);
 
+// Get Author and Book - (body: [authorId]) - (authorId is required)
 router.post('/getAuthorAndBook', authorValidator.getAuthorAndBook);
 
 module.exports = router;
