e7fff49a9d72f7b4f2be3d26b3ace22418058478
httpskedar <32976533+httpskedar@users.noreply.github.com>
2020-05-15 00:54:00 +0530

    Update book.controller.js

--- app/Components/BookComponent/book.controller.js
+++ app/Components/BookComponent/book.controller.js
@@ -1,15 +1,15 @@
 const bookUtility = require('./book.utility');
 
-const {Book} = require('../../../config/database');
+const {Book, Author} = require('../../../config/database');
 
 exports.addBook = function (req, res) {
     Book.create(req.body)
-        .then((result) => {    
+        .then((result) => {
             res.status(200).send({message: 'Book saved successfully!'});
         }).catch(error => {
-            console.log(error);  
-            res.status(200).send({message: 'Book not saved!'});
-        });
+        console.log(error);
+        res.status(200).send({message: 'Book not saved!'});
+    });
 };
 
 exports.updateBook = function (req, res) {
@@ -40,31 +40,27 @@ exports.deleteBook = function (req, res) {
     });
 };
 
-exports.getBook = function (req, res) {
-    Book.count({
-        where: {
-            bookId: req.body.bookId
-        }
+exports.getAllBook = function (req, res) {
+    Book.findAll({
+        include: [{
+            model: Author,
+        }]
     }).then(result => {
-        if (result === 0) {
-            res.status(200).send({message: 'Book not found'});
-        } else {
-            bookUtility.getBook(req, res);
-        }
+        res.status(200).send({result});
     });
 };
 
-// exports.getAuthorAndBook = function (req, res) {
-//     Author.count({
-//         where: {
-//             authorId: req.body.authorId
-//         }
-//     }).then(result => {
-//         if (result === 0) {
-//             res.status(204).send({message: 'Author not found'});
-//         } else {
-//             authorUtility.getAuthorAndBook(req, res);
-//         }
-//     });
-// };
+exports.getBookAndAuthor = function (req, res) {
+    Author.count({
+        where: {
+            authorId: req.body.authorId
+        }
+    }).then(result => {
+        if (result === 0) {
+            res.status(204).send({message: 'Author not found'});
+        } else {
+            authorUtility.getBookAndAuthor(req, res);
+        }
+    });
+};
 
