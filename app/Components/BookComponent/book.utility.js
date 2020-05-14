15ceca251d86fb4358ab2156d7aac8a54a3f6fce
httpskedar <32976533+httpskedar@users.noreply.github.com>
2020-05-15 00:54:28 +0530

    Update book.utility.js

--- app/Components/BookComponent/book.utility.js
+++ app/Components/BookComponent/book.utility.js
@@ -1,48 +1,38 @@
-const {Book} = require('../../../config/database');
+const {Book, Author} = require('../../../config/database');
 
 exports.updateBook = function (req, res) {
     Book.update({
-            bookName: req.body.bookName //,
-            // authorId: req.body.authorId
-        },{ 
-            where: {bookId: req.body.bookId}
-        }).then((result) => {    
-            res.status(200).send({message: 'Book updated successfully!'});
-        }).catch(error => {
-            console.log(error);  
-            res.status(204).send({message: 'Book not updated!'});
-        });
+        bookName: req.body.bookName //,
+        // authorId: req.body.authorId
+    }, {
+        where: {bookId: req.body.bookId}
+    }).then((result) => {
+        res.status(200).send({message: 'Book updated successfully!'});
+    }).catch(error => {
+        console.log(error);
+        res.status(204).send({message: 'Book not updated!'});
+    });
 };
 
 exports.deleteBook = function (req, res) {
     Book.destroy({
-        where: { bookId: req.body.bookId }
+        where: {bookId: req.body.bookId}
     }).then(() => {
         res.status(200).send({message: 'Book deleted successfully!'});
     }).catch(error => {
-        console.log(error);  
+        console.log(error);
         res.status(204).send({message: 'Book not deleted!'});
     });
 
 };
 
-exports.getBook = function (req, res) {
-    Book.findOne({
-    where: {
-        bookId: req.body.bookId
-    }
+exports.getBookAndAuthor = function (req, res) {
+    Book.findAll({
+        include: [{
+            model: Author,
+            where: {authorId: req.body.authorId}
+        }]
     }).then(result => {
         res.status(200).send({result});
     });
 };
-
-// exports.getAuthorAndBook = function (req, res) {
-//     Author.findAll({
-//         include: [{
-//             model: Book,
-//             where: { authorId: req.body.authorId }
-//         }]
-//     }).then(result => {
-//         res.status(200).send({result});
-//     });
-// };
